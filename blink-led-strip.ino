5c9edffa6703ed83116e4d1f2f97edc1520a6868
Samuel Mann <samuelcmann@gmail.com>
2020-02-24 21:05:28 +0100

    attach interrupt on left button

--- blink-led-strip.ino
+++ blink-led-strip.ino
@@ -5,8 +5,8 @@ const Pixel pix = Pixel();
 
 const int SOLO_LED_PIN = 8;
 
-const int BUTTON_LEFT_PIN = 9;
-const int BUTTON_RIGHT_PIN = 10;
+const int BUTTON_LEFT_PIN = 2;
+const int BUTTON_RIGHT_PIN = 3;
 int buttonLeftState = 0;
 int buttonRightState = 0;
 
@@ -18,30 +18,25 @@ int nextLedOnIndex = 0;
 const long intervalTurnOnNextLed = 250;
 
 const int NUM_LEDS = 3;
-const int LED_PIN = 2;
+const int LED_PIN = 9;
 
 Pixel pixels[NUM_LEDS];
 CRGB leds[NUM_LEDS];
-
-const CRGB ON = CRGB(255, 255, 0);
-const CRGB OFF = CRGB(0, 0, 0);
-
-void blinkLed() {
-  digitalWrite(SOLO_LED_PIN, HIGH);
-  delay(50);
-  digitalWrite(SOLO_LED_PIN, LOW);
-}
 
 // SETUP ************************************************************
 
 void setup() {
   Serial.begin(9600);
-  
-  // setup solo LED pin
-  pinMode(SOLO_LED_PIN, OUTPUT);
 
   // setup button pins
   pinMode(BUTTON_LEFT_PIN, INPUT);
+  
+  attachInterrupt(
+    digitalPinToInterrupt(BUTTON_LEFT_PIN),
+    toggleLeftSignal,
+    RISING
+  );
+  
   pinMode(BUTTON_RIGHT_PIN, INPUT);
 
   // fill up array of pixels
@@ -63,56 +58,9 @@ void setup() {
 
 // LOOP ************************************************************
 
-//void loop() {
-//
-//  buttonLeftState = digitalRead(BUTTON_LEFT_PIN);
-//
-//  if (buttonLeftState == HIGH) {
-//    if (isLeftBlinking == true){
-//      isLeftBlinking = false;
-//    } else {
-//      isLeftBlinking = true;
-//    }
-//    
-//  }
-//
-//  if (isLeftBlinking == true){
-//    
-//    for (int i = 0; i < NUM_LEDS; i++) {
-//      // turn on current led
-//      pixels[i].setIsOn(true);
-//      leds[i] = pixels[i].color;
-//      FastLED.show();
-//      delay(intervalTurnOnNextLed);
-//    }
-//
-//    for (int i = 0; i < NUM_LEDS; i++) {
-//      pixels[i].setIsOn(false);
-//      leds[i] = pixels[i].color;
-//    }
-//    FastLED.show();
-//    delay(intervalTurnOnNextLed);
-//  }
-//}
-
 void loop() {
 
-  buttonLeftState = digitalRead(BUTTON_LEFT_PIN);
-
-  if (buttonLeftState == HIGH) {
-    if (isLeftBlinking == true){
-      isLeftBlinking = false;
-    } else {
-      isLeftBlinking = true;
-    }
-    
-  }
-
-  Serial.print("isLeftBlinking: ");
-  Serial.println(isLeftBlinking);
-  Serial.println("************");
-
-  if (isLeftBlinking == true){
+  if (isLeftBlinking == true) {
     unsigned long currentMillis = millis();
     
     for (int i = 0; i < NUM_LEDS; i++) {
@@ -144,7 +92,25 @@ void loop() {
       prevLedOnMillis = currentMillis;
       nextLedOnIndex = 0;
     }
+  } else {
+    for (int i = 0; i < NUM_LEDS; i++) {
+      pixels[i].setIsOn(false);
+      leds[i] = pixels[i].color;
+    }
   }
   
   FastLED.show();
+}
+
+// FUNCTIONS ************************************************************
+
+void toggleLeftSignal() {
+  Serial.println("toggleLeftSignal");
+  isLeftBlinking = !isLeftBlinking;
+  prevLedOnMillis = 0;
+  nextLedOnIndex = 0;
+}
+
+void toggleRightSignal() {
+  isRightBlinking = !isRightBlinking;
 }
