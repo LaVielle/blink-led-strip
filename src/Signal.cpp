189bddbcd49599e24367e76a5669d3cc40a33b87
Samuel Mann <samuelcmann@gmail.com>
2020-03-26 10:24:21 +0100

    basic blink each led through one strip

--- src/Signal.cpp
+++ src/Signal.cpp
@@ -29,25 +29,40 @@ void Signal::blink(){
 
     if (_isBlinking) {
         unsigned long currentMillis = millis();
-
-        const bool timeReached = currentMillis - _prevLedOnMillis >= _intervalTurnOnNextLed;
-
-        if (timeReached) {
-            Serial.println("time reached");
-
-            _prevLedOnMillis = currentMillis;
             
-            for (int i = 0; i < _numLeds; i++) {
-                Pixel p = pixels[i];
-                bool isOn = p.getIsOn();
-                p.setIsOn(!isOn);
+        for (int i = 0; i < _numLeds; i++) {
 
-                pixels[i] = p;
+            const bool timeReached = currentMillis - _prevLedOnMillis >= _intervalTurnOnNextLed;
+            const bool isCorrectIndex = i == _nextLedOnIndex;
 
-                leds[i] = p.getColor();
+            if (timeReached && isCorrectIndex) {
+                // turn on current led
+                pixels[i].setIsOn(true);
+                leds[i] = pixels[i].getColor();
+
+                // set turn on time and index for next led
+                _prevLedOnMillis = currentMillis;
+                _nextLedOnIndex = i + 1;
             }
         }
 
-        FastLED.show();
+        const bool timeToResetReached = currentMillis - _prevLedOnMillis >= _intervalTurnOnNextLed;
+        
+        if (timeToResetReached && _nextLedOnIndex >= _numLeds) {
+            for (int i = 0; i < _numLeds; i++) {
+                pixels[i].setIsOn(false);
+                leds[i] = pixels[i].getColor();
+            }
+
+            _prevLedOnMillis = currentMillis;
+            _nextLedOnIndex = 0;
+        }
+    } else {
+        for (int i = 0; i < _numLeds; i++) {
+            pixels[i].setIsOn(false);
+            leds[i] = pixels[i].getColor();
+        }
     }
+
+    FastLED.show();
 }
